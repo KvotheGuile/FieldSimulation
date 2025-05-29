c0ba65176933be315b99bf7ec883d0b9caac5f38
KvotheGuile <kvothejoestar@gmail.com>
2025-05-28 23:31:52 -0700

    electric field function

--- particle_group.py
+++ particle_group.py
@@ -7,6 +7,14 @@ class ParticleManager:
         self.particles: list[Particle] = []
 
     def electric_field_at_point(self, point: list[float]) -> list[float]:
+        field_total = [0, 0]
+
+        if len(self.particles) < 1: return field_total
+
+        for particle_i in self.particles:
+            field_i = particle_i.electric_field_at(point)
+            field_total = [field_total[0] + field_i[0],
+                           field_total[1] + field_i[1]]
 
         return point
 
